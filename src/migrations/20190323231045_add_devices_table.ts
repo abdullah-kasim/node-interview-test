8f5e2ff08df8ab557c1471cc25ab78dca5ab6608
abdullah-kasim <abdullah.kasim.123@gmail.com:q>
2019-03-23 23:17:15 +0800

    feat(auth): add `devices` table migration

---
+++ src/migrations/20190323231045_add_devices_table.ts
@@ -0,0 +1,39 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  await knex.schema.createTable('devices', (t) => {
+    t.increments('id')
+      .primary()
+      .notNullable()
+
+    t.integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('cascade')
+      .notNullable()
+
+    t.string('refresh_token')
+      .notNullable()
+
+    t.string('type')
+      .notNullable()
+
+    t.string('firebase_token')
+      .nullable()
+
+    t.string('device_id')
+      .notNullable()
+
+    t.unique(['user_id', 'device_id'])
+
+    t.timestamp('expire_at')
+      .notNullable()
+    t.timestamps()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+}
+
