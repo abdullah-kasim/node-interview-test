7a0386b42b2e5155f57be611c3297900637eac46
abdullah-kasim <abdullah.kasim.123@gmail.com:q>
2019-03-30 18:23:02 +0800

    feat(auth): add two devices per user for the seeder

--- src/seeders/seed_users_and_devices.ts
+++ src/seeders/seed_users_and_devices.ts
@@ -12,28 +12,25 @@ exports.seed = async function(knex: Knex): Promise<any> {
   const devices = [DeviceType.BROWSER, DeviceType.MOBILE];
 
   const promises = [...new Array(5).keys()].map(async key => {
-    const tokens = await Promise.all([
-      AuthService.hashPassword('password123456'),
-      AuthService.createRefreshToken(),
-      RandomHelper.randomString(50)
-    ]);
-    const deviceType = devices[key % 2];
-
     const user = await User.create({
       nickname: `User${key + 1}`,
       email: `user${key + 1}@getnada.com`,
-      password: tokens[0]
+      password: await AuthService.hashPassword('password123456')
     });
-    const device = await Device.create({
-      user_id: user.id,
-      refresh_token: tokens[1],
-      type: deviceType,
-      firebase_token: null,
-      device_id: tokens[2],
-      expire_at: moment()
-        .add(1, 'year')
-        .toDate()
+    const expireAt = moment()
+      .add(1, 'year')
+      .toDate();
+    const devicesToCreate = [...new Array(2).keys()].map(async key => {
+      return await Device.create({
+        user_id: user.id,
+        refresh_token: await AuthService.createRefreshToken(),
+        type: devices[key % 2],
+        firebase_token: null,
+        device_id: await RandomHelper.randomString(), // very error-prone
+        expire_at: expireAt
+      });
     });
+    await Promise.all(devicesToCreate);
     return user;
   });
 
