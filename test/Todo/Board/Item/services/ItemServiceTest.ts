8b856edd2a41bde43a294ed2b827c03cf8d86c7b
abdullah-kasim <abdullah.kasim.123@gmail.com:q>
2019-03-31 23:37:17 +0800

    feat(todo): add stubs for item and board test

---
+++ test/Todo/Board/Item/services/ItemServiceTest.ts
@@ -0,0 +1,31 @@
+import test from "ava"
+import sinon from "sinon"
+import { MailerService } from "../../../../../src/services/MailerService"
+
+test.serial("get all todo items from the board", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("get a single item from the board", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("create an item and return its details", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("errors out when non-members try to create", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("triggers a firebase push notification on create", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("syncs an item and returns it with the new content", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("deletes an item", async t => {
+  const email = "test@example.com"
+})
---
+++ test/Todo/Board/services/BoardServiceTest.ts
@@ -0,0 +1,24 @@
+import test from "ava"
+import sinon from "sinon"
+import { User } from "../../../../src/models/User"
+
+test.serial("creates the board with the creator as the owner", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("adds user to the board", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("disallow non-owners from adding or removing users", async t => {
+  // we test the middleware instead
+  const email = "test@example.com"
+})
+
+test.serial("removes the user from the board", async t => {
+  const email = "test@example.com"
+})
+
+test.serial("get all boards for the user", async t => {
+  const email = "test@example.com"
+})
