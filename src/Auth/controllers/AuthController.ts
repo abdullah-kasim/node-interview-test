0b6dd44c954daaeb670b9d7ad57ad333e6a7c7b9
abdullah-kasim <abdullah.kasim.123@gmail.com:q>
2019-03-31 14:58:51 +0800

    feat(base): add api for updating firebase cloud token

--- src/Auth/controllers/AuthController.ts
+++ src/Auth/controllers/AuthController.ts
@@ -1,101 +1,86 @@
-import Joi from 'joi';
-import { DefaultRequestHandler } from '../../customTypes/fastify';
-import { ResponseHelper } from '../../helpers/ResponseHelper';
-import { AuthService } from '../services/AuthService';
-import { DeviceType } from '../../models/Device';
-import { passwordValidation } from '../../settings/validate';
+import Joi from "joi"
+import { DefaultRequestHandler } from "../../customTypes/fastify"
+import { ResponseHelper } from "../../helpers/ResponseHelper"
+import { AuthService } from "../services/AuthService"
+import { DeviceType } from "../../models/Device"
+import { getJoi, passwordValidation } from "../../settings/validate"
+import { DeviceRepository } from "../../repositories/DeviceRepository"
 
 interface RegisterBody {
-  password: string;
-  email: string;
-  nickname: string;
+  password: string
+  email: string
+  nickname: string
 }
 
 interface LoginBody {
-  email: string;
-  password: string;
-  type: DeviceType;
-  deviceId: string;
-  firebaseCloudToken?: string;
+  email: string
+  password: string
+  type: DeviceType
+  deviceId: string
+  firebaseCloudToken?: string
 }
 
 interface LogoutBody {
-  all: boolean;
+  all: boolean
 }
 
 interface RefreshBody {
-  refreshToken: string;
+  refreshToken: string
+}
+
+interface UpdateFirebaseCloudTokenBody {
+  firebaseCloudToken: string
+  refreshToken: string
 }
 
 export class AuthController {
-  static registerUsingFirebase: DefaultRequestHandler<
-    any,
-    any,
-    any,
-    any
-  > = async (request, reply) => {
+  static registerUsingFirebase: DefaultRequestHandler<any, any, any, any> = async (request, reply) => {
     const schema = Joi.object().keys({
       nickname: Joi.string().required(),
       firebaseToken: Joi.string().required()
-    });
+    })
     const validation = Joi.validate(request.body, schema, {
       abortEarly: false
-    });
+    })
     if (validation.error !== null) {
-      return ResponseHelper.validationError(request, reply, validation.error);
+      return ResponseHelper.validationError(request, reply, validation.error)
     }
-    const { nickname, firebaseToken } = request.body;
+    const { nickname, firebaseToken } = request.body
     try {
-      await AuthService.registerUsingFirebase(nickname, firebaseToken);
-      return ResponseHelper.ok(request, reply);
+      await AuthService.registerUsingFirebase(nickname, firebaseToken)
+      return ResponseHelper.ok(request, reply)
     } catch (e) {
-      return ResponseHelper.validationError(request, reply, e);
+      return ResponseHelper.validationError(request, reply, e)
     }
-  };
+  }
 
-  static loginUsingFirebase: DefaultRequestHandler<any, any, any, any> = async (
-    request,
-    reply
-  ) => {
+  static loginUsingFirebase: DefaultRequestHandler<any, any, any, any> = async (request, reply) => {
     const schema = Joi.object().keys({
       deviceId: Joi.string().required(),
       type: Joi.string().required(),
       firebaseToken: Joi.string().required(),
       firebaseCloudToken: Joi.string().optional()
-    });
+    })
     const validation = Joi.validate(request.body, schema, {
       abortEarly: false
-    });
+    })
     if (validation.error !== null) {
-      return ResponseHelper.validationError(request, reply, validation.error);
+      return ResponseHelper.validationError(request, reply, validation.error)
     }
 
-    const {
-      deviceId,
-      firebaseToken = null,
-      firebaseCloudToken,
-      type
-    } = request.body;
+    const { deviceId, firebaseToken = null, firebaseCloudToken, type } = request.body
     try {
-      const tokenDetails = await AuthService.loginUsingFirebase(
-        firebaseToken,
-        type,
-        deviceId,
-        firebaseCloudToken
-      );
+      const tokenDetails = await AuthService.loginUsingFirebase(firebaseToken, type, deviceId, firebaseCloudToken)
       return ResponseHelper.create(request, reply, {
         ...tokenDetails,
         user: AuthService.cleanUser(tokenDetails.user)
-      });
+      })
     } catch (e) {
-      return ResponseHelper.validationError(request, reply, e);
+      return ResponseHelper.validationError(request, reply, e)
     }
-  };
+  }
 
-  static register: DefaultRequestHandler<any, any, any, RegisterBody> = async (
-    request,
-    reply
-  ) => {
+  static register: DefaultRequestHandler<any, any, any, RegisterBody> = async (request, reply) => {
     const schema = Joi.object().keys({
       password: passwordValidation.required(),
       email: Joi.string()
@@ -103,116 +88,118 @@ export class AuthController {
         .email()
         .required(),
       nickname: Joi.string().required()
-    });
+    })
     const validation = Joi.validate(request.body, schema, {
       abortEarly: false
-    });
+    })
     if (validation.error !== null) {
-      return ResponseHelper.validationError(request, reply, validation.error);
+      return ResponseHelper.validationError(request, reply, validation.error)
     }
-    const { email, nickname, password } = request.body;
+    const { email, nickname, password } = request.body
     try {
-      await AuthService.register(nickname, email, password);
-      return ResponseHelper.ok(request, reply);
+      await AuthService.register(nickname, email, password)
+      return ResponseHelper.ok(request, reply)
     } catch (e) {
-      return ResponseHelper.validationError(request, reply, e);
+      return ResponseHelper.validationError(request, reply, e)
     }
-  };
+  }
 
-  static login: DefaultRequestHandler<any, any, any, LoginBody> = async (
-    request,
-    reply
-  ) => {
+  static login: DefaultRequestHandler<any, any, any, LoginBody> = async (request, reply) => {
     const schema = Joi.object().keys({
       email: Joi.string().required(),
       password: Joi.string().required(),
       deviceId: Joi.string().required(),
       type: Joi.string().required(),
       firebaseCloudToken: Joi.string().optional()
-    });
+    })
     const validation = Joi.validate(request.body, schema, {
       abortEarly: false
-    });
+    })
     if (validation.error !== null) {
-      return ResponseHelper.validationError(request, reply, validation.error);
+      return ResponseHelper.validationError(request, reply, validation.error)
     }
 
-    const {
-      email,
-      password,
-      deviceId,
-      firebaseCloudToken = null,
-      type
-    } = request.body;
+    const { email, password, deviceId, firebaseCloudToken = null, type } = request.body
     try {
-      const tokenDetails = await AuthService.login(
-        email,
-        password,
-        type,
-        deviceId,
-        firebaseCloudToken
-      );
+      const tokenDetails = await AuthService.login(email, password, type, deviceId, firebaseCloudToken)
       return ResponseHelper.create(request, reply, {
         ...tokenDetails,
         user: AuthService.cleanUser(tokenDetails.user)
-      });
+      })
     } catch (e) {
-      return ResponseHelper.validationError(request, reply, e);
+      return ResponseHelper.validationError(request, reply, e)
     }
-  };
+  }
 
-  static logout: DefaultRequestHandler<any, any, any, LogoutBody> = async (
-    request,
-    reply
-  ) => {
+  static logout: DefaultRequestHandler<any, any, any, LogoutBody> = async (request, reply) => {
     const schema = Joi.object().keys({
       all: Joi.bool().required()
-    });
+    })
     const validation = Joi.validate(request.body, schema, {
       abortEarly: false
-    });
+    })
     if (validation.error !== null) {
-      return ResponseHelper.validationError(request, reply, validation.error);
+      return ResponseHelper.validationError(request, reply, validation.error)
     }
 
-    const user = await AuthService.getUserFromRequest(request);
+    const user = await AuthService.getUserFromRequest(request)
     if (!user) {
       return ResponseHelper.validationError(request, reply, {
-        user: 'User is not logged in'
-      });
+        user: "User is not logged in"
+      })
     }
 
     if (request.body.all) {
-      await AuthService.logoutAll(user);
+      await AuthService.logoutAll(user)
     } else {
-      await AuthService.logout(user, user.devices[0]);
+      await AuthService.logout(user, user.devices[0])
     }
-    return ResponseHelper.ok(request, reply);
-  };
+    return ResponseHelper.ok(request, reply)
+  }
 
-  static refresh: DefaultRequestHandler<any, any, any, RefreshBody> = async (
+  static updateFirebaseCloudToken: DefaultRequestHandler<any, any, any, UpdateFirebaseCloudTokenBody> = async (
     request,
     reply
   ) => {
+    const schema = getJoi()
+      .object()
+      .keys({
+        firebaseCloudToken: getJoi()
+          .string()
+          .required(),
+        refreshToken: getJoi()
+          .string()
+          .required()
+      })
+    const validation = schema.validate(request.body.refreshToken, {
+      abortEarly: false
+    })
+    if (validation.error !== null) {
+      return ResponseHelper.validationError(request, reply, validation.error)
+    }
+
+    const device = await DeviceRepository.getDeviceByRefreshToken(request.body.refreshToken)
+    await AuthService.updateFirebaseCloudToken(device, request.body.firebaseCloudToken)
+  }
+
+  static refresh: DefaultRequestHandler<any, any, any, RefreshBody> = async (request, reply) => {
     const schema = Joi.object().keys({
       refreshToken: Joi.string().required()
-    });
+    })
     const validation = Joi.validate(request.body, schema, {
       abortEarly: false
-    });
+    })
     if (validation.error !== null) {
-      return ResponseHelper.validationError(request, reply, validation.error);
+      return ResponseHelper.validationError(request, reply, validation.error)
     }
     try {
-      const tokenDetails = await AuthService.renewRefreshToken(
-        request.body.refreshToken
-      );
+      const tokenDetails = await AuthService.renewRefreshToken(request.body.refreshToken)
       return ResponseHelper.create(request, reply, {
         ...tokenDetails,
         user: AuthService.cleanUser(tokenDetails.user)
-      });
+      })
     } catch (e) {
-      return ResponseHelper.validationError(request, reply, e);
+      return ResponseHelper.validationError(request, reply, e)
     }
-  };
+  }
 }
