79824f11386c2a8ad867f4f11f97bfe6e80dce09
abdullah-kasim <abdullah.kasim.123@gmail.com:q>
2019-03-31 23:33:32 +0800

    feat(auth): add unit tests for reset password

--- src/Auth/ResetPassword/services/ResetPasswordService.ts
+++ src/Auth/ResetPassword/services/ResetPasswordService.ts
@@ -1,27 +1,27 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as util from 'util';
-import querystring from 'querystring';
-import * as _ from 'lodash';
-import moment from 'moment';
-import { User } from '../../../models/User';
-import { MailerService } from '../../../services/MailerService';
-import { env } from '../../../settings/env';
-import { CryptHelper } from '../../../helpers/CryptHelper';
-import { UserRepository } from '../../../repositories/UserRepository';
-import { EmailNotFound } from '../../services/exceptions/EmailNotFound';
-import { AuthService } from '../../services/AuthService';
-import { InvalidResetPasswordToken } from './exceptions/InvalidResetPasswordToken';
+import * as path from "path"
+import * as fs from "fs"
+import * as util from "util"
+import querystring from "querystring"
+import * as _ from "lodash"
+import moment from "moment"
+import { User } from "../../../models/User"
+import { MailerService } from "../../../services/MailerService"
+import { env } from "../../../settings/env"
+import { CryptHelper } from "../../../helpers/CryptHelper"
+import { UserRepository } from "../../../repositories/UserRepository"
+import { EmailNotFound } from "../../services/exceptions/EmailNotFound"
+import { AuthService } from "../../services/AuthService"
+import { InvalidResetPasswordToken } from "./exceptions/InvalidResetPasswordToken"
 
 // Move this to another class
 enum TokenType {
-  RESET_PASSWORD = 'RESET_PASSWORD'
+  RESET_PASSWORD = "RESET_PASSWORD"
 }
 
 interface Token {
-  email: string;
-  type: string;
-  expireAt: number;
+  email: string
+  type: string
+  expireAt: number
 }
 
 export class ResetPasswordService {
@@ -30,72 +30,69 @@ export class ResetPasswordService {
       where: {
         email
       }
-    });
-    return Boolean(user);
-  };
+    })
+    return Boolean(user)
+  }
 
   static sendResetEmail = async email => {
-    const emailExists = ResetPasswordService.validateEmailExists(email);
+    const emailExists = await ResetPasswordService.validateEmailExists(email)
     if (!emailExists) {
-      throw new EmailNotFound();
+      throw new EmailNotFound()
     }
-    const readFile = util.promisify(fs.readFile);
-    const templateString = await readFile(
-      path.resolve(__dirname, '../templates/emails/resetEmail.ejs'),
-      'utf8'
-    );
+    const readFile = util.promisify(fs.readFile)
+    const templateString = await readFile(path.resolve(__dirname, "../templates/emails/resetEmail.ejs"), "utf8")
     await MailerService.sendMail({
       to: email,
       html: _.template(templateString)({
         resetLink: ResetPasswordService.createResetLink(email)
       })
-    });
-  };
+    })
+  }
 
-  static createResetLink = email => {
+  static createResetLink = (email): string => {
     const queryString = querystring.stringify({
       email,
       type: TokenType.RESET_PASSWORD,
       token: ResetPasswordService.createToken(email)
-    });
-    return `${env.FRONTEND_URL}/reset?${queryString}`;
-  };
+    })
+    return `${env.FRONTEND_URL}/reset?${queryString}`
+  }
 
   static createToken = email => {
     const payload = {
       email,
       type: TokenType.RESET_PASSWORD,
       expireAt: moment()
-        .add(1, 'week')
+        .add(1, "week")
         .unix()
-    };
-    const text = JSON.stringify(payload);
-    return CryptHelper.encrypt(text);
-  };
+    }
+    const text = JSON.stringify(payload)
+    return CryptHelper.encrypt(text)
+  }
 
   static decryptToken = (token): Token => {
-    let payload: Token = null;
+    let payload: Token = null
     try {
-      payload = JSON.parse(CryptHelper.decrypt(token)) as Token;
+      payload = JSON.parse(CryptHelper.decrypt(token)) as Token
     } catch (error) {
-      throw new InvalidResetPasswordToken(error.message);
+      throw new InvalidResetPasswordToken(error.message)
     }
 
     // if the current time, is past the expire date
-    const now = moment().unix();
+    const now = moment().unix()
     if (now > payload.expireAt) {
       // then the token is invalid.
-      throw new InvalidResetPasswordToken();
+      throw new InvalidResetPasswordToken()
     }
-    return payload;
-  };
+    return payload
+  }
 
   static resetPassword = async (newPassword: string, token: string) => {
-    const tokenPayload = ResetPasswordService.decryptToken(token);
+    const tokenPayload = ResetPasswordService.decryptToken(token)
 
-    const user = await UserRepository.getUserByEmail(tokenPayload.email);
-    user.password = await AuthService.hashPassword(newPassword);
-    await user.save();
-    return user;
-  };
+    const user = await UserRepository.getUserByEmail(tokenPayload.email)
+    user.password = await AuthService.hashPassword(newPassword)
+    await user.save()
+    return user
+  }
 }
---
+++ test/Auth/ResetPassword/services/ResetPasswordServiceTest.ts
@@ -0,0 +1,84 @@
+import querystring from "querystring"
+import test from "ava"
+import sinon from "sinon"
+import moment from "moment"
+import { User } from "../../../../src/models/User"
+import { Device } from "../../../../src/models/Device"
+import { AuthService } from "../../../../src/Auth/services/AuthService"
+import { MailerService } from "../../../../src/services/MailerService"
+import { ResetPasswordService } from "../../../../src/Auth/ResetPassword/services/ResetPasswordService"
+import { UserRepository } from "../../../../src/repositories/UserRepository"
+
+test.serial("send reset e-mail if e-mail exists", async t => {
+  const email = "test@example.com"
+  const sendMail = sinon.stub(MailerService, "sendMail").resolves()
+  sinon.stub(ResetPasswordService, "validateEmailExists").resolves(true)
+  const createResetLink = sinon.stub(ResetPasswordService, "createResetLink").returns("someResetLink")
+  await ResetPasswordService.sendResetEmail(email)
+  const call = sendMail.getCalls()[0]
+  const arg = call.args[0]
+  t.true(arg.to === "test@example.com")
+  t.true(createResetLink.calledWith("test@example.com"))
+})
+
+test.serial("throws error if e-mail does not exist", async t => {
+  const email = "test@example.com"
+  const sendMail = sinon.stub(MailerService, "sendMail").resolves()
+  sinon.stub(ResetPasswordService, "validateEmailExists").resolves(false)
+  const createResetLink = sinon.stub(ResetPasswordService, "createResetLink").returns("someResetLink")
+  try {
+    await ResetPasswordService.sendResetEmail(email)
+    t.fail()
+  } catch (e) {
+    t.pass()
+  }
+})
+
+test.serial("generates a proper reset link", async t => {
+  const email = "test@example.com"
+  const sendMail = sinon.stub(MailerService, "sendMail").resolves()
+  sinon.stub(ResetPasswordService, "createToken").returns("someToken")
+  const createResetLink = sinon.stub(ResetPasswordService, "createResetLink").returns("someResetLink")
+  // how do we test that the query string is valid?
+  const resetLink = ResetPasswordService.createResetLink(email)
+  const queryStringPayload = querystring.parse(resetLink.split("?")[1])
+  t.true(queryStringPayload.email === "test@example.com")
+  t.true(queryStringPayload.type === "RESET_PASSWORD")
+  t.true(queryStringPayload.token === "someToken")
+})
+
+test.serial("resets the password, given the token and password", async t => {
+  const sendMail = sinon.stub(MailerService, "sendMail").resolves()
+  sinon.stub(ResetPasswordService, "decryptToken").returns({
+    type: "RESET_PASSWORD",
+    email: "test@example.com",
+    expireAt: moment().unix()
+  })
+  const user = sinon.createStubInstance(User)
+  user.password = "someOldHash"
+  sinon.stub(UserRepository, "getUserByEmail").resolves(user as any)
+  sinon.stub(AuthService, "hashPassword").resolves("someBcryptHash")
+
+  const result = await ResetPasswordService.resetPassword("newPassword", "someToken")
+  t.truthy(result)
+  t.true(user.save.called)
+  t.true(result.password === "someBcryptHash")
+})
+
+test.serial("throws an error, given the wrong token", async t => {
+  const sendMail = sinon.stub(MailerService, "sendMail").resolves()
+  sinon.stub(ResetPasswordService, "decryptToken").throws()
+  const user = sinon.createStubInstance(User)
+  user.password = "someOldHash"
+  sinon.stub(UserRepository, "getUserByEmail").resolves(user as any)
+  sinon.stub(AuthService, "hashPassword").resolves("someBcryptHash")
+
+  try {
+    await ResetPasswordService.resetPassword("newPassword", "someToken")
+    t.fail()
+  } catch (e) {
+    t.true(user.password === "someOldHash")
+    t.false(user.save.called)
+    t.pass()
+  }
+})
